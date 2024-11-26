4c8a243c66d5368b6e74c5c0ba9ade03aa01e3f7
Ronald Marques <ronaldpmarques01@gmail.com>
2024-11-26 15:46:02 -0300

    Criação da lógica de login

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_login import LoginManager, login_user, current_user
 from models.user import User
 from database import db
 
@@ -6,7 +7,34 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 db.init_app(app)
+
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({"message": "Login realizado com sucesso"})
+
+    return jsonify({"message": "Credenciais invalidas"}), 400
+
 
 if __name__ == "__main__":
     app.run(debug=True)
